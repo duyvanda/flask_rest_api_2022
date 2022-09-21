234d5632df1a92725ec9d1486dd9bc9fba1ef999
Duy Van <vanquangduy10@gmail.com>
2022-09-21 22:33:29 +0700

    jwt

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required, current_identity
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, JWTManager
+from hmac import compare_digest
 
-from security import authenticate, identity
+from security import username_table, userid_table
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
-app.secret_key = 'jose'
+app.config["JWT_SECRET_KEY"] = "123phanam"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = False  # Change this!
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity, )
+# jwt = JWT(app, authenticate, identity)
+# jwt.JWT_AUTH_HEADER_PREFIX = "JWT"
 
-items = []
+jwt = JWTManager(app)
+
+items = [{'name': "Duy", 'price': 1.5}]
 
 class Item(Resource):
     parser = reqparse.RequestParser()
@@ -53,12 +58,47 @@ class Item(Resource):
             item.update(data)
         return item
 
+
 class ItemList(Resource):
+
+    @jwt_required()
     def get(self):
         return {'items': items}
 
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('password',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+
+class UserLogin(Resource):
+    def post(self):
+        data = _user_parser.parse_args()
+
+        user = username_table.get(data['username'], None)
+
+        # this is what the `authenticate()` function did in security.py
+        if user and compare_digest(user.password, data['password']):
+            # identity= is what the identity() function did in security.py—now stored in the JWT
+            access_token = create_access_token(identity=user.id, fresh=True) 
+            refresh_token = create_refresh_token(user.id)
+            return {
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }, 200
+
+        return {"message": "Invalid Credentials!"}, 401
+
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     app.run(debug=True)  # important to mention debug=True
--- security.py
+++ security.py
@@ -14,11 +14,12 @@ users = [
 username_table = {u.username: u for u in users}
 userid_table = {u.id: u for u in users}
 
-def authenticate(username, password):
-    user = username_table.get(username, None)
-    if user and compare_digest(user.password, password):
-        return user
+# def authenticate(username, password):
+#     user = username_table.get(username, None)
+#     if user and compare_digest(user.password, password):
+#         return user
 
-def identity(payload):
-    user_id = payload['identity']
-    return userid_table.get(user_id, None)
+# def identity(payload):
+#     user_id = payload['identity']
+#     return userid_table.get(user_id, None)
+
